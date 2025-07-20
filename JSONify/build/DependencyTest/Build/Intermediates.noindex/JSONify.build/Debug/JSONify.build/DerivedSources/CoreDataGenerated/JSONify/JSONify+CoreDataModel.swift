e2dd397f0e1947328af77fa695aff7f6e8024900
andy-zhangtao <ztao8607@gmail.com>
2025-07-20 18:10:29 +0800

    refactor: 调整JSONHealer依赖为本地路径
    - 将JSONHealer依赖从../../JSONHealer改为./JSONHealer
    - 添加依赖测试脚本

---
+++ JSONify/build/DependencyTest/Build/Intermediates.noindex/JSONify.build/Debug/JSONify.build/DerivedSources/CoreDataGenerated/JSONify/JSONify+CoreDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  JSONify+CoreDataModel.swift
+//  
+//
+//  Created by 张涛 on 7/20/25.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
